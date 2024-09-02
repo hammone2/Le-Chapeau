35e6c8350ee2b2db8054b23b0f917e1eb3ba5c58
hammone2 <hammone2@my.erau.edu>
2024-09-02 13:09:06 -0700

    Network Setup

---
+++ Le Chapeau/Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class NetworkManager : MonoBehaviourPunCallbacks
+{
+    // instance
+    public static NetworkManager instance;
+
+    void Awake()
+    {
+        /// if an instance already exists and its not this then destroy
+        if (instance != null && instance != this)
+            gameObject.SetActive(false);
+        else
+        {
+            // set the instance
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        
+    }
+
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Connected to master server");
+        CreateRoom("testroom");
+    }
+
+    public void CreateRoom (string roomName)
+    {
+        PhotonNetwork.CreateRoom(roomName);
+    }
+
+    public override void OnCreatedRoom()
+    {
+        Debug.Log("Created Room: " + PhotonNetwork.CurrentRoom.Name);
+    }
+
+    public void JoinRoom(string roomName)
+    {
+        PhotonNetwork.JoinRoom(roomName);
+    }
+
+    public void ChangeScene (string sceneName)
+    {
+        PhotonNetwork.LoadLevel(sceneName);
+    }
+}
