6ccb6c73c691e853e54c9f4305d8349fd273a7af
hammone2 <hammone2@my.erau.edu>
2024-09-06 08:45:07 -0700

    player controller

---
+++ Le Chapeau/Assets/Scripts/GameManager.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using System.Linq;
+
+public class GameManager : MonoBehaviourPunCallbacks
+{
+    public bool gameEnded = false;
+    public float timeToWin;
+    public float invincibleDuration;
+    private float hatPickupTime;
+
+    public string playerPrefabLocation;
+    public Transform[] spawnPoints;
+    public PlayerController[] players;
+    public int playerWithHat;
+    private int playersInGame;
+
+    public static GameManager instance;
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    void Start()
+    {
+        players = new PlayerController[PhotonNetwork.PlayerList.Length];
+        photonView.RPC("ImInGame", RpcTarget.AllBuffered);
+    }
+
+    [PunRPC]
+    void ImInGame()
+    {
+        playersInGame++;
+
+        if (playersInGame == PhotonNetwork.PlayerList.Length)
+            SpawnPlayer();
+    }
+
+    void SpawnPlayer()
+    {
+        GameObject playerObj = PhotonNetwork.Instantiate(playerPrefabLocation, spawnPoints[Random.Range(0, spawnPoints.Length)].position, Quaternion.identity);
+        PlayerController playerScript = playerObj.GetComponent<PlayerController>();
+    }
+}
